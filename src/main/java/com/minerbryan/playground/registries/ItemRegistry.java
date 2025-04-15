120d56dd2231f9c2f3567bc80324eadfb9a54bb4
Bryan <bdjetton@optonline.net>
2025-04-15 10:23:20 -0400

    Fixed registerBlockItem method

--- src/main/java/com/minerbryan/playground/registries/ItemRegistry.java
+++ src/main/java/com/minerbryan/playground/registries/ItemRegistry.java
@@ -4,7 +4,6 @@ import com.minerbryan.playground.PlaygroundMod;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -48,12 +47,12 @@ public class ItemRegistry {
     public static final DeferredItem<BlockItem> NEBULA_FENCE_GATE_ITEM = ITEMS.register("nebula_fence_gate", () -> new BlockItem(BlockRegistry.NEBULA_FENCE_GATE.get(), new Item.Properties()));
 
     //Doors
-    public static final DeferredItem<BlockItem> NEBULA_DOOR_ITEM = registerBlockItem("nebula_door", BlockRegistry.NEBULA_DOOR.get());
-    public static final DeferredItem<BlockItem> STELLARIUM_DOOR_ITEM = registerBlockItem("stellarium_door", BlockRegistry.STELLARIUM_DOOR.get());
+    public static final DeferredItem<BlockItem> NEBULA_DOOR_ITEM = registerBlockItem("nebula_door", BlockRegistry.NEBULA_DOOR);
+    public static final DeferredItem<BlockItem> STELLARIUM_DOOR_ITEM = registerBlockItem("stellarium_door", BlockRegistry.STELLARIUM_DOOR);
 
     //Trapdoors
-    public static final DeferredItem<BlockItem> NEBULA_TRAPDOOR_ITEM = registerBlockItem("nebula_trapdoor", BlockRegistry.NEBULA_TRAPDOOR.get());
-    public static final DeferredItem<BlockItem> STELLARIUM_TRAPDOOR_ITEM = registerBlockItem("stellarium_trapdoor", BlockRegistry.STELLARIUM_TRAPDOOR.get());
+    public static final DeferredItem<BlockItem> NEBULA_TRAPDOOR_ITEM = registerBlockItem("nebula_trapdoor", BlockRegistry.NEBULA_TRAPDOOR);
+    public static final DeferredItem<BlockItem> STELLARIUM_TRAPDOOR_ITEM = registerBlockItem("stellarium_trapdoor", BlockRegistry.STELLARIUM_TRAPDOOR);
 
 
     // Creates a new food item with the id "playground:example_id", nutrition 1 and saturation 2
@@ -64,8 +63,8 @@ public class ItemRegistry {
     public static final DeferredItem<Item> STELLARIUM_INGOT = ITEMS.register("stellarium_ingot", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> RAW_STELLARIUM = ITEMS.register("raw_stellarium", () -> new Item(new Item.Properties()));
 
-    protected static DeferredItem<BlockItem> registerBlockItem(String name, Block block) {
-        return ITEMS.register(name, () -> new BlockItem(block, new Item.Properties()));
+    protected static DeferredItem<BlockItem> registerBlockItem(String name, DeferredBlock<?> block) {
+        return ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
     public static void register(IEventBus bus){
         ITEMS.register(bus);
