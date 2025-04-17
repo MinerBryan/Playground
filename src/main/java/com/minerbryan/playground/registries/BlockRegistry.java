8cbdcddbef68e61d02dbbc8b7243c1006bc16b00
Bryan <bdjetton@optonline.net>
2025-04-17 11:50:32 -0400

    Removed Block Item registration, updated Block registration

--- src/main/java/com/minerbryan/playground/registries/BlockRegistry.java
+++ src/main/java/com/minerbryan/playground/registries/BlockRegistry.java
@@ -2,6 +2,7 @@ package com.minerbryan.playground.registries;
 
 import com.minerbryan.playground.PlaygroundMod;
 import com.minerbryan.playground.blocks.ExampleBlockEntity;
+import com.minerbryan.playground.registries.helpers.BlockRegistryHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.entity.EntityType;
@@ -19,157 +20,70 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+
 import java.util.Optional;
+
+//import static com.minerbryan.playground.registries.helpers.BlockRegistryHelper.registerTrapdoor;
 
 
 //rawr
-public class BlockRegistry {
+public class BlockRegistry extends BlockRegistryHelper{
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(PlaygroundMod.MODID);
 
     // Creates a new Block with the id "playground:example_block", combining the namespace and path
-    public static final DeferredBlock<Block> BISMUTH_BLOCK = BLOCKS.registerSimpleBlock("bismuth_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
-    public static final DeferredBlock<Block> EXAMPLE_BLOCK_ENTITY = BLOCKS.register("example_block_entity", () -> new ExampleBlockEntity(BlockBehaviour.Properties.of().noOcclusion()));
+    public static final DeferredBlock<Block> BISMUTH_BLOCK = registerBasicBlock("bismuth_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
+    public static final DeferredBlock<Block> EXAMPLE_BLOCK_ENTITY = registerBlock("example_block_entity", () -> new ExampleBlockEntity(BlockBehaviour.Properties.of().noOcclusion()));
 
     //Ore
-    public static final DeferredBlock<Block> STELLARIUM_ORE = BLOCKS.register("stellarium_ore", () -> new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST_CLUSTER)));
+    public static final DeferredBlock<Block> STELLARIUM_ORE = registerBlock("stellarium_ore", () -> new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST_CLUSTER)));
 
     //Raw Blocks
-    public static final DeferredBlock<Block> RAW_STELLARIUM_BLOCK = BLOCKS.register("raw_stellarium_block", () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.RAW_IRON_BLOCK)));
+    public static final DeferredBlock<Block> RAW_STELLARIUM_BLOCK = registerBasicBlock("raw_stellarium_block", BlockBehaviour.Properties.ofFullCopy(Blocks.RAW_IRON_BLOCK).mapColor(MapColor.COLOR_BLUE));
 
     //Metal Block
-    public static final DeferredBlock<Block> STELLARIUM_BLOCK = BLOCKS.registerSimpleBlock("stellarium_block", BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK));
+    public static final DeferredBlock<Block> STELLARIUM_BLOCK = registerBasicBlock("stellarium_block", BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).mapColor(MapColor.COLOR_BLUE));
 
     //Planks
-    public static final DeferredBlock<Block> NEBULA_PLANKS = BLOCKS.registerSimpleBlock("nebula_planks", BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS));
+    public static final DeferredBlock<Block> NEBULA_PLANKS = registerBasicBlock("nebula_planks", BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS).mapColor(MapColor.COLOR_PURPLE));
 
     //Sapling
-    public static final DeferredBlock<Block> NEBULA_SAPLING = BLOCKS.register("nebula_sapling",
-            () -> new SaplingBlock(
-                    new TreeGrower(
-                            PlaygroundMod.MODID+":nebula",
-                            0.1F,
-                            Optional.empty(),
-                            Optional.empty(),
-                            Optional.of(ConfigFeatureRegistry.NEBULA_KEY),
-                            Optional.empty(),
-                            Optional.empty(),
-                            Optional.empty()
-                    ),
-                    BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_SAPLING)
-            )
+    public static final DeferredBlock<SaplingBlock> NEBULA_SAPLING = registerSapling("nebula_sapling",
+            new TreeGrower(
+                    PlaygroundMod.MODID+":nebula",
+                    0.1F,
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.of(ConfigFeatureRegistry.NEBULA_KEY),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty()
+            ),
+            BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_SAPLING).mapColor(MapColor.COLOR_MAGENTA)
     );
 
     //Log
-    public static final DeferredBlock<Block> NEBULA_LOG = BLOCKS.register("nebula_log", () -> new RotatedPillarBlock(
-            BlockBehaviour.Properties.of()
-                    .mapColor(MapColor.COLOR_PURPLE)
-                    .instrument(NoteBlockInstrument.BASS)
-                    .strength(2.0f)
-                    .sound(SoundType.WOOD)
-                    .ignitedByLava()
-            )
-    );
+    public static final DeferredBlock<Block> NEBULA_LOG = registerBlock("nebula_log", () -> new RotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LOG).mapColor(MapColor.COLOR_PURPLE)));
 
     //Wood
-    public static final DeferredBlock<Block> NEBULA_WOOD = BLOCKS.register("nebula_wood", () -> new RotatedPillarBlock(
-                    BlockBehaviour.Properties.of()
-                            .mapColor(MapColor.COLOR_PURPLE)
-                            .instrument(NoteBlockInstrument.BASS)
-                            .strength(2.0f)
-                            .sound(SoundType.WOOD)
-                            .ignitedByLava()
-            )
-    );
+    public static final DeferredBlock<Block> NEBULA_WOOD = registerBlock("nebula_wood", () -> new RotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_WOOD).mapColor(MapColor.COLOR_PURPLE)));
 
     //Leaves
-    public static final DeferredBlock<Block> NEBULA_LEAVES = BLOCKS.register("nebula_leaves", () -> new LeavesBlock(
-            BlockBehaviour.Properties.of()
-                    .mapColor(MapColor.COLOR_MAGENTA)
-                    .forceSolidOn()
-                    .strength(2.0f)
-                    .randomTicks()
-                    .sound(SoundType.AZALEA_LEAVES)
-                    .noOcclusion()
-                    .isValidSpawn(Blocks::ocelotOrParrot)
-                    .isSuffocating(BlockRegistry::never)
-                    .isViewBlocking(BlockRegistry::never)
-                    .ignitedByLava()
-                    .pushReaction(PushReaction.DESTROY)
-                    .isRedstoneConductor(BlockRegistry::never)
-        )
-    );
+    public static final DeferredBlock<LeavesBlock> NEBULA_LEAVES = registerLeaves("nebula_leaves", BlockBehaviour.Properties.ofFullCopy(Blocks.AZALEA_LEAVES).mapColor(MapColor.COLOR_MAGENTA));
 
     //Fences
-    public static final DeferredBlock<Block> NEBULA_FENCE = BLOCKS.register(
-            "nebula_fence",
-            () -> new FenceBlock(
-                    BlockBehaviour.Properties.of()
-                            .mapColor(NEBULA_PLANKS.get().defaultMapColor())
-                            .forceSolidOn()
-                            .instrument(NoteBlockInstrument.BASS)
-                            .strength(2.0F, 3.0F)
-                            .sound(SoundType.WOOD)
-                            .ignitedByLava()
-            )
-    );
+    public static final DeferredBlock<FenceBlock> NEBULA_FENCE = registerFence("nebula_fence", BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_FENCE).mapColor(MapColor.COLOR_PURPLE));
 
     //Fence Gates
-    public static final DeferredBlock<Block> NEBULA_FENCE_GATE = BLOCKS.register(
-            "nebula_fence_gate",
-            () -> new FenceGateBlock(
-                    WoodType.OAK,
-                    BlockBehaviour.Properties.of()
-                            .mapColor(NEBULA_PLANKS.get().defaultMapColor())
-                            .forceSolidOn()
-                            .instrument(NoteBlockInstrument.BASS)
-                            .strength(2.0F,3.0F)
-                            .ignitedByLava()
-            )
-    );
+    public static final DeferredBlock<FenceGateBlock> NEBULA_FENCE_GATE = registerFenceGate("nebula_fence_gate", WoodType.OAK,BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_FENCE_GATE).mapColor(MapColor.COLOR_PURPLE));
 
     //Doors
-    public static final DeferredBlock<Block> NEBULA_DOOR = BLOCKS.register(
-            "nebula_door",
-            () -> new DoorBlock(
-                    BlockSetType.CHERRY,
-                    BlockBehaviour.Properties.of()
-                            .mapColor(NEBULA_PLANKS.get().defaultMapColor())
-                            .instrument(NoteBlockInstrument.BASS)
-                            .strength(3.0F)
-                            .noOcclusion()
-                            .ignitedByLava()
-                            .pushReaction(PushReaction.DESTROY)
-            )
-    );
-    public static final DeferredBlock<Block> STELLARIUM_DOOR = BLOCKS.register(
-            "stellarium_door",
-            () -> new DoorBlock(
-                    BlockSetType.IRON,
-                    BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_DOOR)
-            )
-    );
+    public static final DeferredBlock<DoorBlock> NEBULA_DOOR = registerDoor("nebula_door", BlockSetType.CHERRY, BlockBehaviour.Properties.ofFullCopy(Blocks.CHERRY_DOOR).mapColor(MapColor.COLOR_PURPLE));
+    public static final DeferredBlock<DoorBlock> STELLARIUM_DOOR = registerDoor("stellarium_door", BlockSetType.IRON, BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_DOOR));
 
     //Trapdoors
-    public static final DeferredBlock<Block> NEBULA_TRAPDOOR = BLOCKS.register(
-            "nebula_trapdoor",
-            () -> new TrapDoorBlock(
-                    BlockSetType.CHERRY,
-                    BlockBehaviour.Properties.of()
-                            .mapColor(NEBULA_PLANKS.get().defaultMapColor())
-                            .instrument(NoteBlockInstrument.BASS)
-                            .strength(3.0F)
-                            .noOcclusion()
-                            .ignitedByLava()
-                            .isValidSpawn(BlockRegistry::never)
-            )
-    );
-    public static final DeferredBlock<Block> STELLARIUM_TRAPDOOR = BLOCKS.register(
-            "stellarium_trapdoor",
-            () -> new TrapDoorBlock(
-                    BlockSetType.IRON,
-                    BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_TRAPDOOR)
-            )
-    );
+    public static final DeferredBlock<TrapDoorBlock> NEBULA_TRAPDOOR = registerTrapdoor("nebula_trapdoor", BlockSetType.CHERRY, BlockBehaviour.Properties.ofFullCopy(Blocks.CHERRY_TRAPDOOR).mapColor(MapColor.COLOR_PURPLE));
+    public static final DeferredBlock<TrapDoorBlock> STELLARIUM_TRAPDOOR = registerTrapdoor("stellarium_trapdoor", BlockSetType.IRON, BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_TRAPDOOR));
+
     // Directly Copied over from MC's Blocks.java file
     private static boolean never(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, EntityType<?> entityType) { return false; }
     private static boolean never(BlockState state, BlockGetter blockGetter, BlockPos pos) {
--- src/main/java/com/minerbryan/playground/registries/ItemRegistry.java
+++ src/main/java/com/minerbryan/playground/registries/ItemRegistry.java
@@ -12,49 +12,6 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ItemRegistry {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(PlaygroundMod.MODID);
 
-    // Creates a new BlockItem with the id "playground:example_block", combining the namespace and path
-    public static final DeferredItem<BlockItem> BISMUTH_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("bismuth_block", BlockRegistry.BISMUTH_BLOCK);
-    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ENTITY_ITEM = ITEMS.register("example_block_entity", () -> new BlockItem(BlockRegistry.EXAMPLE_BLOCK_ENTITY.get(), new Item.Properties()));
-
-    //Ores
-    public static final DeferredItem<BlockItem> STELLARIUM_ORE_ITEM = registerBlockItem("stellarium_ore", BlockRegistry.STELLARIUM_ORE);
-
-    //Raw Blocks
-    public static final DeferredItem<BlockItem> RAW_STELLARIUM_BLOCK_ITEM = registerBlockItem("raw_stellarium_block", BlockRegistry.RAW_STELLARIUM_BLOCK);
-
-    //Metal Blocks
-    public static final DeferredItem<BlockItem> STELLARIUM_BLOCK_ITEM = registerBlockItem("stellarium_block", BlockRegistry.STELLARIUM_BLOCK);
-
-    //Sapling
-    public static final DeferredItem<BlockItem> NEBULA_SAPLING_ITEM = registerBlockItem("nebula_sapling", BlockRegistry.NEBULA_SAPLING);
-
-    //Planks
-    public static final DeferredItem<BlockItem> NEBULA_PLANKS_ITEM = registerBlockItem("nebula_planks", BlockRegistry.NEBULA_PLANKS);
-
-    //Log
-    public static final DeferredItem<BlockItem> NEBULA_LOG_ITEM = registerBlockItem("nebula_log", BlockRegistry.NEBULA_LOG);
-
-    //Wood
-    public static final DeferredItem<BlockItem> NEBULA_WOOD_ITEM = registerBlockItem("nebula_wood", BlockRegistry.NEBULA_WOOD);
-
-    //Leaves
-    public static final DeferredItem<BlockItem> NEBULA_LEAVES_ITEM = registerBlockItem("nebula_leaves", BlockRegistry.NEBULA_LEAVES);
-
-    //Fence
-    public static final DeferredItem<BlockItem> NEBULA_FENCE_ITEM = registerBlockItem("nebula_fence", BlockRegistry.NEBULA_FENCE);
-
-    //Fence Gate
-    public static final DeferredItem<BlockItem> NEBULA_FENCE_GATE_ITEM = registerBlockItem("nebula_fence_gate", BlockRegistry.NEBULA_FENCE_GATE);
-
-    //Doors
-    public static final DeferredItem<BlockItem> NEBULA_DOOR_ITEM = registerBlockItem("nebula_door", BlockRegistry.NEBULA_DOOR);
-    public static final DeferredItem<BlockItem> STELLARIUM_DOOR_ITEM = registerBlockItem("stellarium_door", BlockRegistry.STELLARIUM_DOOR);
-
-    //Trapdoors
-    public static final DeferredItem<BlockItem> NEBULA_TRAPDOOR_ITEM = registerBlockItem("nebula_trapdoor", BlockRegistry.NEBULA_TRAPDOOR);
-    public static final DeferredItem<BlockItem> STELLARIUM_TRAPDOOR_ITEM = registerBlockItem("stellarium_trapdoor", BlockRegistry.STELLARIUM_TRAPDOOR);
-
-
     // Creates a new food item with the id "playground:example_id", nutrition 1 and saturation 2
     public static final DeferredItem<Item> RADISH = ITEMS.registerSimpleItem("radish", new Item.Properties().food(new FoodProperties.Builder()
             .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
@@ -64,7 +21,7 @@ public class ItemRegistry {
     public static final DeferredItem<Item> RAW_STELLARIUM = ITEMS.register("raw_stellarium", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> STELLARIUM_NUGGET = ITEMS.register("stellarium_nugget", () -> new Item(new Item.Properties()));
 
-    protected static DeferredItem<BlockItem> registerBlockItem(String name, DeferredBlock<?> block) {
+    public static DeferredItem<BlockItem> registerBlockItem(String name, DeferredBlock<?> block) {
         return ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
     public static void register(IEventBus bus){
