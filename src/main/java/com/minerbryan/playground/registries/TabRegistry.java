c2e23632071388131411faac95e47767f6400f0e
Bryan <bdjetton@optonline.net>
2025-04-17 11:51:07 -0400

    Blocks are added into the creative tab via iterating over the registry list

--- src/main/java/com/minerbryan/playground/registries/TabRegistry.java
+++ src/main/java/com/minerbryan/playground/registries/TabRegistry.java
@@ -5,14 +5,19 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.Collection;
+
+import static com.minerbryan.playground.registries.BlockRegistry.BLOCKS;
 
 public class TabRegistry {
     // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "playground" namespace
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, PlaygroundMod.MODID);
-
 
     // Creates a creative tab with the id "playground:example_tab" for the example item, that is placed after the combat tab
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = TABS.register(
@@ -22,26 +27,14 @@ public class TabRegistry {
                 .withTabsBefore(CreativeModeTabs.COMBAT)
                 .icon(() -> ItemRegistry.RADISH.get().getDefaultInstance())
                 .displayItems((parameters, output) -> {
+                    BLOCKS.getEntries().forEach(holder -> {
+                        output.accept(holder.get());
+                    });
+
                     output.accept(ItemRegistry.RADISH.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-                    output.accept(BlockRegistry.BISMUTH_BLOCK.get());
-                    output.accept(BlockRegistry.EXAMPLE_BLOCK_ENTITY.get());
-                    output.accept(BlockRegistry.STELLARIUM_ORE.get());
                     output.accept(ItemRegistry.RAW_STELLARIUM.get());
                     output.accept(ItemRegistry.STELLARIUM_NUGGET.get());
                     output.accept(ItemRegistry.STELLARIUM_INGOT.get());
-                    output.accept(BlockRegistry.RAW_STELLARIUM_BLOCK.get());
-                    output.accept(BlockRegistry.STELLARIUM_BLOCK.get());
-                    output.accept(BlockRegistry.STELLARIUM_DOOR.get());
-                    output.accept(BlockRegistry.STELLARIUM_TRAPDOOR.get());
-                    output.accept(BlockRegistry.NEBULA_SAPLING.get());
-                    output.accept(BlockRegistry.NEBULA_LEAVES.get());
-                    output.accept(BlockRegistry.NEBULA_LOG.get());
-                    output.accept(BlockRegistry.NEBULA_WOOD.get());
-                    output.accept(BlockRegistry.NEBULA_PLANKS.get());
-                    output.accept(BlockRegistry.NEBULA_FENCE.get());
-                    output.accept(BlockRegistry.NEBULA_FENCE_GATE.get());
-                    output.accept(BlockRegistry.NEBULA_DOOR.get());
-                    output.accept(BlockRegistry.NEBULA_TRAPDOOR.get());
                 })
                 .build()
     );
@@ -49,11 +42,4 @@ public class TabRegistry {
     public static void register(IEventBus bus){
         TABS.register(bus);
     }
-
-    // Add the example block item to the building blocks tab
-//    private void addCreative(BuildCreativeModeTabContentsEvent event)
-//    {
-//        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-//            event.accept(EXAMPLE_BLOCK_ITEM);
-//    }
 }
